a0b7104512b4ee0e9f81b074e88bba9028d02864
hamza-766 <hamzags872@gmail.com>
2024-09-11 22:03:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADrw06NTu1pQLAhG5Wb7NcCS_FAFY5xpA",
+    authDomain: "testing-hamza.firebaseapp.com",
+    projectId: "testing-hamza",
+    storageBucket: "testing-hamza.appspot.com",
+    messagingSenderId: "637460893559",
+    appId: "1:637460893559:web:47c46d7d37d85503c15b72",
+    measurementId: "G-0F4V8R0Q4K"
+  };
